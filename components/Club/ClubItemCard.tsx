57bd13752e0f5377c6e7e512475ec996e8007d2d
Eungyeol <eungyeole@naver.com>
2020-12-28 14:14:05 +0900

    styled-components 설정

---
+++ components/Club/ClubItemCard.tsx
@@ -0,0 +1,5 @@
+
+function ClubItemCard(){
+
+}
+export default ClubItemCard
---
+++ components/Club/ClubItemList.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles"
+function ClubItemList(){
+    return(
+        <div>
+            <S.Test>나는 행복해</S.Test>
+        </div>
+    )
+}
+export default ClubItemList
