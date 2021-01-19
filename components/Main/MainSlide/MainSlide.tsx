3cffc28d7e21d627f3ab0548d7e9e4b645bbfcab
eungyeole <eungyeole@naver.com>
2021-01-20 03:09:53 +0900

    UPDATE ; MainSlide 경로 수정

--- components/Main/MainSlide/MainSlide.tsx
+++ components/Main/MainSlide/MainSlide.tsx
@@ -1,27 +1,7 @@
 import * as s from './styles'
-import { useState} from 'react';
+import { useState } from 'react';
 import useInterval from '../../../hooks/index'
-
-const Menu =()=>{
-    return(
-        <s.MenuBar>
-            <s.TitleMenu>전공 동아리</s.TitleMenu>
-            <s.DetailMenu>웹</s.DetailMenu>
-            <s.DetailMenu>앱</s.DetailMenu>
-            <s.DetailMenu>임베디드  </s.DetailMenu>
-            <s.DetailMenu>게임</s.DetailMenu>
-            <s.DetailMenu>인공지능</s.DetailMenu>
-            <s.DetailMenu>정보보안</s.DetailMenu>
-            <s.TitleMenu>창체 동아리</s.TitleMenu>
-            <s.DetailMenu>교과목</s.DetailMenu>
-            <s.DetailMenu>전공과목</s.DetailMenu>
-            <s.DetailMenu>스포츠</s.DetailMenu>
-            <s.DetailMenu>예술 창의 교양</s.DetailMenu>
-            <s.DetailMenu>교내활동</s.DetailMenu>
-            <s.DetailMenu>기타</s.DetailMenu>   
-        </s.MenuBar>
-    )
-}
+import Menu from '../Menu/Menu';
 
 const MainSlide =()=>{
     const ClubData = [
