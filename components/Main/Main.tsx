cad1ad54ad1e028a1762acc0afc1e79f7809e62e
eungyeole <eungyeole@naver.com>
2021-01-20 03:11:49 +0900

    CREATE ; Main Index 추가

---
+++ components/Main/Main.tsx
@@ -0,0 +1,10 @@
+import Feed from "./Feed/Feed"
+import MainSlide from "./MainSlide/MainSlide"
+
+const Main = () => (
+    <>
+        <MainSlide></MainSlide>
+        <Feed></Feed>
+    </>
+)
+export default Main
