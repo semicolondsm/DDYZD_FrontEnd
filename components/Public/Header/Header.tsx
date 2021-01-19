037f49a94b128f8a94aaaca51285fb85ceb481eb
jiwon <jidole041214@naver.com>
2021-01-19 15:08:52 +0900

    UPDATE ; 색깔 유동적 변경 구현

--- components/Public/Header/Header.tsx
+++ components/Public/Header/Header.tsx
@@ -1,26 +1,32 @@
-import * as h from './HeaderStyle'
-
-function Header(e:any){
+import React from 'react';
+import * as h from './styles'
+import Link from "next/link"
+import ListIco from './ListIco';
+import  Image from "next/image"
+function Header({color} : {color:string}){
     return(
         <div>
         <h.Header>
-            <h.TopHeader style={{backgroundColor:(e.e==1)?"white":"#350871"}}>
-                <a style={{color:(e.e==1)?"black":"white"}}>대동여지도</a>
+            <h.TopHeader color={color}>
+                <Link href="/">
+                    <a><Image src="/Logo.png" width={25} height={25}></Image>대동여지도</a>
+                </Link>
             </h.TopHeader>
             <h.BottomHeader>
-                <div style={{width:"70%"}}>
+                <ul>
+                    <ListIco></ListIco>
                     <h3>전체 카테고리</h3>
-                    <a>동아리 소개</a>
-                    <a>동아리 신청</a>
-                    <a>동아리 게시물</a>
-                    <a>동아리 물품 신청</a>
-                    <a>공지사항</a>
-                </div>
-                <div style={{width:"20%", marginRight:"12%"}}>
-                    <a>이명호 님</a>
-                    <a>로그아웃</a>
-                    <a>고객센터</a>
-                </div>
+                    <li>동아리 소개</li>
+                    <li>동아리 신청</li>
+                    <li>동아리 게시물</li>
+                    <li>동아리 물품 신청</li>
+                    <li>공지사항</li>
+                </ul>
+                <ul>
+                    <li>이명호 님</li>
+                    <li>로그아웃</li>
+                    <li>고객센터</li>
+                </ul>
             </h.BottomHeader>
         </h.Header>
         </div>
