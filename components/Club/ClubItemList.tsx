7b0b4fc7dd7a0b48bd42cedb88ffd7f9723bf9f7
eungyeole <eungyeole@naver.com>
2021-01-19 17:19:55 +0900

    UPDATE; ClubItemList 조건 단순화

--- components/Club/ClubItemList.tsx
+++ components/Club/ClubItemList.tsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { ClubItemTableData } from "../../interfaces";
 import ClubItemTable from "./ClubItemTable"
@@ -6,10 +5,26 @@ import * as S from "./styles"
 
 function ClubItemList(){
     const [Data,setData] = useState<ClubItemTableData[] | null>(null);
+    useEffect(()=>{
+        setData([
+            {
+                user_name: "안은결",
+                data: [{
+                    item_name : "test",
+                    item_state : "test",
+                    option : "test",
+                    link : "test",
+                    amount: "3",
+                    price: 5000,
+                    delivery_status: "test",
+                }]
+            }
+        ])
+    },[])
     return(
         <S.Container>
             {
-                Data && Data.map((i)=>{
+                Data?.map((i)=>{
                     console.log(i);
                     return(<ClubItemTable props={i}></ClubItemTable>)
                 })
