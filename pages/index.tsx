2c111a41754c8d3be53d96fbeca2193d616163ba
eungyeole <eungyeole@naver.com>
2021-01-19 16:24:22 +0900

    UPDATE ; Pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Header from '../components/Public/Header/Header'
 import MainSlide from '../components/Main/MainSlide'
-
+import Feed from "../components/Main/Feed"
 const Index = () => {
   return(
     <>
     <Header color="white"></Header>
     <MainSlide></MainSlide>
+    <Feed></Feed>
     </>
   )
   
