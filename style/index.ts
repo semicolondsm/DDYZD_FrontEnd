ea8df3453a71199f2a226b2d888e9e404301d6a4
eungyeole <eungyeole@naver.com>
2021-01-19 23:32:24 +0900

    CREATE ; Color Style

---
+++ style/index.ts
@@ -0,0 +1,14 @@
+export const color = {
+    grey100 : "#F5F5F5",
+    grey200 : "#00000029",
+    grey300 : "#C8C8C8",
+    grey400 : "#A4A4A4",
+    grey500 : "#707070",
+    grey600 : "#545454",
+    grey700 : "#3D3D3D",
+    purple100 : "#A45EE1",
+    purple200 : "#713EFF",
+    purple300 : "#350871",
+    yellow : "#FFE874",
+    
+}
