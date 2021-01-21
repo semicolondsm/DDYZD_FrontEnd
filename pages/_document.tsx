1801b667ff0979d5a231227ae224d7615c257e49
eungyeole <eungyeole@naver.com>
2021-01-22 05:49:40 +0900

    FIX ; Build GlobalStyle Not Working

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,41 +1,37 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import { GlobalStyle } from '../style/GlobalStyled'
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-    try {
-      ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => sheet.collectStyles(
-          <>
-            <GlobalStyle></GlobalStyle>
-            <App {...props} />
-          </>
-        ),
-      })
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-           {initialProps.styles}
-           {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
+import Helmet from "react-helmet";
+type Props = {
+  helmet : any,
+  styleTags : any,
+}
+export default class MyDocument extends Document<Props> {
+  static async getInitialProps(context : DocumentContext) {
+    const sheet = new ServerStyleSheet(); 
+    const page = context.renderPage(App => props =>
+      sheet.collectStyles(
+        <>
+          <GlobalStyle />
+          <App {...props} />
+        </>
+      )
+    ); 
+    const styleTags = sheet.getStyleElement();
+    return { ...page, helmet: Helmet.renderStatic(), styleTags };
   }
   render(){
+    const { htmlAttributes, bodyAttributes, ...helmet } = this.props.helmet as { htmlAttributes : any, bodyAttributes: any}; 
+    const htmlAttrs = htmlAttributes.toComponent();
+    const bodyAttrs = bodyAttributes.toComponent();
     return(
-      <Html>
+      <Html {...htmlAttrs}>
         <Head>
+          {this.props.styleTags}
+          {Object.values(helmet).map(el => el.toComponent())} 
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet"/>
         </Head>
-        <body>
+        <body {...bodyAttrs}>
             <Main />
             <NextScript />
         </body>
