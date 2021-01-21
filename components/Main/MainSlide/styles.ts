a6bf99e4563209224a3a44293397dec523c65877
eungyeole <eungyeole@naver.com>
2021-01-21 21:57:07 +0900

    UPDATE ; 정지원 화이팅

--- components/Main/MainSlide/styles.ts
+++ components/Main/MainSlide/styles.ts
@@ -1,32 +1,6 @@
 import styled from "styled-components"
 import { color } from "../../../style"
-export const MenuBar = styled.ul`
-padding: 0 83px 0 25px;
-height:700px;
-position:absolute;
-background-color:rgb(0,0,0,0.5);
-margin-left:250px;
-color:white;
-min-width:100px;
-display:flex;
-flex-direction:column;
-z-index:30;
-min-width:100px;
-`
 
-export const TitleMenu = styled.h3`
-font-size:17px;
-font-weight:500;
-font-family:sans-serif;
-padding:20px 0 4px 0;
-`
-
-export const DetailMenu = styled.li`
-font-size:14px;
-font-weight:500;
-font-family:sans-serif;
-padding:11px 0;
-`
 export const SlideContainer = styled.div`
 width:100%;
 height:700px;
