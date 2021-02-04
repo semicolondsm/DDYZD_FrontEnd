7226d77bac3a41fba36fc644f09ec7aea7c72d71
jiwon <jidole041214@naver.com>
2021-02-04 21:33:24 +0900

    CREATE : context api

---
+++ public/context/context.tsx
@@ -0,0 +1,47 @@
+import React, { useReducer, useContext, createContext, Dispatch } from 'react';
+
+type State = {
+  modalName: string
+};
+
+type Action =| { type: 'SET_MODAL'; name: string }
+
+type SampleDispatch = Dispatch<Action>;
+
+const SampleStateContext = createContext<State | null>(null);
+const SampleDispatchContext = createContext<SampleDispatch | null>(null);
+
+function reducer(_state: State, action: Action): State {
+  switch (action.type) {
+    case 'SET_MODAL':
+      return {
+        modalName:action.name
+      };
+    default:
+      throw new Error('Unhandled action');
+  }
+}
+
+export function SampleProvider({ children }: { children: React.ReactNode }) {
+  const [state, dispatch] = useReducer(reducer,{modalName:''});
+
+  return (
+    <SampleStateContext.Provider value={state}>
+      <SampleDispatchContext.Provider value={dispatch}>
+        {children}
+      </SampleDispatchContext.Provider>
+    </SampleStateContext.Provider>
+  );
+}
+
+export function useSampleState() {
+  const state = useContext(SampleStateContext);
+  if (!state) throw new Error('Cannot find SampleProvider');
+  return state;
+}
+
+export function useSampleDispatch() {
+  const dispatch = useContext(SampleDispatchContext);
+  if (!dispatch) throw new Error('Cannot find SampleProvider');
+  return dispatch;
+}
