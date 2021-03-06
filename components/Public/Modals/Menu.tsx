2654e76227b8218c2489fe3b98456814f9349c05
jiwon <jidole041214@naver.com>
2021-03-06 17:21:45 +0900

    UPDATE : query string url

--- components/Public/Modals/Menu.tsx
+++ components/Public/Modals/Menu.tsx
@@ -1,6 +1,21 @@
 import * as s from './styles'
+import club from '@/utils/api/club'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/dist/client/router';
 
 const Modal =()=>{
+    const [data,setData] = useState<any>([]);
+    const router = useRouter();
+    const replacePage =(e : any)=>{
+        router.push(`clubinfo?id=${e.target.className}`) 
+    }
+    useEffect(()=>{
+        club.getClubList()
+        .then((res)=>{
+            setData(res.data);
+        })
+    },[])
     return(
         <s.ModalContainer>
             <s.ModalTitle>전체 카테고리</s.ModalTitle>
@@ -14,12 +29,12 @@ const Modal =()=>{
                     <s.CategoryBox>정보보안</s.CategoryBox>
                 </s.CategoryTitle>
                 <s.Flex>
-                    <s.Detail><a>DBSG</a> <a>DMS</a> <a>FLOSIVA</a> <a>LAPIO</a> <a>SEMICOLON;</a> <a>UP</a> <a>UNDEFINED</a> <a>시나브로</a></s.Detail>
-                    <s.Detail><a>DMS</a> <a>GRAM</a> <a>SEMICOLON;</a> <a>UNDEFINED</a> <a>덧셈</a></s.Detail>
-                    <s.Detail><a>ESD</a><a>NONAMED</a><a>VCC</a></s.Detail>
-                    <s.Detail><a>GG</a><a>TEAM QSS</a></s.Detail>
-                    <s.Detail><a>모딥</a></s.Detail>
-                    <s.Detail><a>INFO</a></s.Detail>
+                    <s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "웹" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail>
+                    <s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "앱" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail>
+                    <s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "임베디드" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail>
+                    <s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "게임" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail>
+                    <s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "인공지능" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail>
+                    <s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "정보보안" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail>
                 </s.Flex>
             </s.Category>
 
@@ -33,12 +48,12 @@ const Modal =()=>{
                     <s.CategoryBox>기타 </s.CategoryBox>
                 </s.CategoryTitle>
                 <s.Flex>
-                    <s.Detail><a>ENGLISH MOMENTUM</a> <a>통계의 미학</a> <a>융합과학</a></s.Detail>
-                    <s.Detail><a>AVOCAT</a> <a>DMS</a> <a>ENTRY</a> <a>PICK</a> <a>SEMICOLON;</a></s.Detail>
-                    <s.Detail><a>농구</a><a>배드민턴 A / B / C</a><a>탁구사랑 1 / 2</a></s.Detail>
-                    <s.Detail><a>음악독서일기</a><a>나를 사랑하게 되는 명상</a><a>마소리딩</a><a>명화그리기 1 / 2</a><a>사서탐독 1 / 2</a></s.Detail>
-                    <s.Detail><a>또래상담부</a><a>방송부</a><a>신문부</a><a>가온누리봉사부</a><a>체인지메이커</a></s.Detail>
-                    <s.Detail><a>영화 감상부</a><a>고사성어부</a></s.Detail>
+                    <s.Detail><s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "교과목" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail></s.Detail>
+                    <s.Detail><s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "전공과목" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail></s.Detail>
+                    <s.Detail><s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "스포츠" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail></s.Detail>
+                    <s.Detail><s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "예술" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail></s.Detail>
+                    <s.Detail><s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "교내활동" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail></s.Detail>
+                    <s.Detail><s.Detail>{data.map((res : any)=>{return res.clubtag[0] == "기타" && <a onClick={replacePage} className={res.clubid} key={res.clubid}>{res.clubname}</a>})}</s.Detail></s.Detail>
                 </s.Flex>
             </s.Category>
         </s.ModalContainer>
