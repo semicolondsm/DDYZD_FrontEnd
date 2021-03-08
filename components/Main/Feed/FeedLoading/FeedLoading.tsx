700a92c203c93c5bdd2d975a0cfbdace0a5e80a6
eungyeole <eungyeole@naver.com>
2021-03-08 18:34:06 +0900

    UPDATE ; Loading

---
+++ components/Main/Feed/FeedLoading/FeedLoading.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styles"
+function FeedLoading(){
+    return(
+        <S.svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
+        </S.svg>
+    )
+}
+export default FeedLoading;
