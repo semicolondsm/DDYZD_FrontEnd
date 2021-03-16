c7420e9e7aa497d3540f3c998fedcda2b4739b05
jiwon <jidole041214@naver.com>
2021-03-16 19:19:22 +0900

    FIX : layout

--- components/Club/ClubApply/ClubItemApply.tsx
+++ components/Club/ClubApply/ClubItemApply.tsx
@@ -1,47 +1,47 @@
-import * as S from './styles';
-import CheckItem from "../ClubTable/CheckItem"
+import * as S from "./styles";
+import CheckItem from "../ClubTable/CheckItem";
 
-function ClubItemApply(){
-    return( 
-        <S.BoxCover>
-            <S.Container>
-                <div>
-                    <p>물품 신청하기</p>
-                    <div>TIP</div>
-                </div>
-                <S.line></S.line>
-                <div>
-                    <p>물품 상태 </p>
-                    <CheckItem/>
-                </div>
-                <input type="text" name="" id=""/>
-                <div>
-                    <p>물품명 </p>
-                    <CheckItem/>
-                </div>
-                <input type="text" name="" id=""/>
-                <div>
-                    <p>옵션 선택</p>
-                </div>
-                <select name="" id="">
-                    <option value="">안녕하세요</option>
-                </select>
-                <div>
-                    <p>수량 선택</p>
-                </div>
-                <S.ItemCount>
-                    <section>
-                        <button>-</button>
-                        <div>0</div>
-                        <button>+</button>
-                    </section>
-                    <h4>총 0개</h4>
-                    <h3>120000 원</h3>
-                </S.ItemCount>
-                <S.ApplyButton>신청하기</S.ApplyButton>
-            </S.Container>
-        </S.BoxCover>
-    )
+function ClubItemApply() {
+  return (
+    <S.BoxCover>
+      <S.Container>
+        <S.DesCont>
+          <S.ApplyItem>물품 신청하기</S.ApplyItem>
+          <S.Tip>TIP</S.Tip>
+        </S.DesCont>
+        <S.line></S.line>
+        <S.SmallDesCont>
+          <S.Ment>물품 상태 </S.Ment>
+          <CheckItem />
+        </S.SmallDesCont>
+        <S.ItemInput type="text" name="" id="" />
+        <S.SmallDesCont>
+          <S.Ment>물품명 </S.Ment>
+          <CheckItem />
+        </S.SmallDesCont>
+        <S.ItemInput type="text" name="" id="" />
+        <S.SmallDesCont>
+          <S.Ment>옵션 선택</S.Ment>
+        </S.SmallDesCont>
+        <S.Select name="" id="">
+          <option value="">안녕하세요</option>
+        </S.Select>
+        <S.SmallDesCont>
+          <S.Ment>수량 선택</S.Ment>
+        </S.SmallDesCont>
+        <S.ItemCount>
+          <S.ConutDiv>
+            <S.StatusBtn>-</S.StatusBtn>
+            <S.Status>0</S.Status>
+            <S.StatusBtn>+</S.StatusBtn>
+          </S.ConutDiv>
+          <h4>총 0개</h4>
+          <h3>120000 원</h3>
+        </S.ItemCount>
+        <S.ApplyButton>신청하기</S.ApplyButton>
+      </S.Container>
+    </S.BoxCover>
+  );
 }
 
 export default ClubItemApply;
