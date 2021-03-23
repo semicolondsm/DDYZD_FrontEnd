5eab77728cf9affd2a81e2f46f2c3a5684a289b6
eungyeole <eungyeole@naver.com>
2021-03-23 09:32:49 +0900

    CREATE ; Channel Talk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Header from "../components/Public/Header/Header";
 import Main from "../components/Main";
-
+import { useEffect } from "react";
+import channel from "../utils/channel"
 const Index = () => {
+  useEffect(()=>{
+    channel("a74d213c-6d20-4295-bf99-cbfde9ca2fe9");
+  },[])
   return (
     <>
       <Header color="white"></Header>
