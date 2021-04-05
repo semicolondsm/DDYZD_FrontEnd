a69086b29d20613231b412ad53ddec0559f9f3b8
eungyeole <eungyeole@naver.com>
2021-04-05 22:55:14 +0900

    UPDATE ; directoryStructure

---
+++ src/components/Public/Modals/index.tsx
@@ -0,0 +1,18 @@
+import FeedModal from '@/src/components/Public/Modals/Feed';
+import {getState} from '../../../context/context'
+
+const ReturnModal=()=>{
+    const state = getState();
+    const name = state.modalName;
+    return(
+        <>
+        {
+            name == "" ? <></>
+            :name == "feed" ? <FeedModal></FeedModal>
+            : <></>
+        }
+        </>
+    )
+}
+
+export default ReturnModal;
